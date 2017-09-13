974691c49fce146959155096ee441bae2273c9e8
Ryan Currie <ryan.currie@gmail.com>
2017-09-13 09:33:38 -0700

    Completed basic search function #1

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var validZips = ['12345', '11223', '54321']
+
+function search(zip) {
+  for (var i = 0; i < validZips.length; i++) {
+    if (zip === validZips[i]) {
+      window.location.href = 'doctors.html'
+    }
+    else {
+      return 'Fail'
+      /* window.location.href = 'sorry.html' */
+    }
+  }
+}
+
+function getUserZip() {
+  var userZip = document.querySelector('#zip').value
+  return userZip
+}
+
+var $checkZip = document.querySelector('#hero-search-button')
+
+$checkZip.addEventListener('click', function(event) {
+  event.preventDefault()
+  search(getUserZip())
+})
