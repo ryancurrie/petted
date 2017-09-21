4eb34413b060189ff0396db8e8497990eaecab59
Ryan Currie <ryan.currie@gmail.com>
2017-09-21 14:34:39 -0700

    Prepped for heroku.

---
+++ public/js/init.js
@@ -0,0 +1,44 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  });
+})(jQuery);
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+$('.datepicker').pickadate({
+  selectMonths: true,
+  selectYears: 15,
+  today: 'Today',
+  clear: 'Clear',
+  close: 'Ok',
+  closeOnSelect: false
+});
+
+$('.timepicker').pickatime({
+  default: 'now',
+  fromnow: 0,
+  twelvehour: true,
+  donetext: 'OK',
+  cleartext: 'Clear',
+  canceltext: 'Cancel',
+  autoclose: false,
+  ampmclickable: true,
+  aftershow: function(){}
+});
+
+$(document).ready(function() {
+  $('select').material_select();
+});
+
+$(document).ready(function() {
+   Materialize.updateTextFields();
+ });
