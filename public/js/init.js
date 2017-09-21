e9e984eba069a00bfc3e3b2e7abede1662ce43cd
Ryan Currie <ryan.currie@gmail.com>
2017-09-21 15:49:12 -0700

    Added cart abandon event.

--- public/js/init.js
+++ public/js/init.js
@@ -51,3 +51,25 @@ $(document).ready(function() {
 $(document).ready(function() {
    Materialize.updateTextFields();
  });
+
+window.addEventListener('close', cartAbandon())
+
+ function cartAbandon() {
+   if (window.location.pathname === '/doctors.html' || '/pet.html' || '/appointment.html' || '/profile.html') {
+     analytics.track('CartAbandon', {
+       userVet: localStorage.getItem('userVet'),
+       userFirstName: localStorage.getItem('userFirstName'),
+       userLastName: localStorage.getItem('userLastName'),
+       userEmail: localStorage.getItem('userEmail'),
+       userPhone: localStorage.getItem('userPhone'),
+       userZip: localStorage.getItem('userZip'),
+       userAddress: localStorage.getItem('userStreetAddress'),
+       userPetName: localStorage.getItem('userPetName'),
+       userPetSpecies: localStorage.getItem('userPetSpecies'),
+       PetBreed: localStorage.getItem('userPetBreed'),
+       PetAge: localStorage.getItem('userPetAge'),
+       appointmentDate: localStorage.getItem('userDate'),
+       appointmentTime: localStorage.getItem('userTime')
+     })
+   }
+ }
